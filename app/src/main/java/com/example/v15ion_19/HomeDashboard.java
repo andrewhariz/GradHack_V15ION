c8e209ac81ee247937b8905e135fef2b60160019
ah16185 <ah16185@essex.ac.uk>
2019-09-18 14:07:33 +0530

    commit unversioned

---
+++ app/src/main/java/com/example/v15ion_19/HomeDashboard.java
@@ -0,0 +1,25 @@
+package com.example.v15ion_19;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.v15ion_19.R;
+
+public class HomeDashboard extends Activity {
+
+    Button settings;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        settings = (Button) findViewById(R.id.settings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+//                startActivity(new Intent(this,Settings.class));
+            }
+        });
+    }
+}
