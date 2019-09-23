319d48efdf72fb860f0d9416eeb8ea9c0f391064
bluekap <45138363+bluekap@users.noreply.github.com>
2019-09-23 18:02:12 +0530

    worked on UI

--- app/src/main/java/com/example/v15ion_19/PaymentActivity.java
+++ app/src/main/java/com/example/v15ion_19/PaymentActivity.java
@@ -6,15 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class PaymentActivity extends AppCompatActivity {
     Button payment;
     ImageButton back;
+    TextView phoneNumber;
+    ImageButton im1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
-
+        phoneNumber = findViewById(R.id.mob);
         payment = findViewById(R.id.PayButton);
         payment.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -28,5 +31,15 @@ public class PaymentActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        im1 = findViewById(R.id.f1);
+        im1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                phoneNumber.setText("9872845236");
+            }
+        });
+
+
+
     }
 }
