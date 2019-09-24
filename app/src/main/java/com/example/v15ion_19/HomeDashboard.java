6755b79928e7456dc1434a94c21467a675ebaa62
ah16185 <ah16185@essex.ac.uk>
2019-09-24 11:07:20 +0530

    added plus button to homepage, only changes button text

--- app/src/main/java/com/example/v15ion_19/HomeDashboard.java
+++ app/src/main/java/com/example/v15ion_19/HomeDashboard.java
@@ -20,29 +20,33 @@ public class HomeDashboard extends Activity {
     static ImageButton settings;
     static Button transfer;
     static Button payment;
+    static Button services;
+    ImageButton plusButton;
     private ViewPager screenPager;
-    PagerAdapter introViewPagerAdapter ;
+    PagerAdapter introViewPagerAdapter;
+    int fontSize;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         final List<ModelDashboard> mList = new ArrayList<>();
-        mList.add(new ModelDashboard(99999,"xxxx xxxx xxxx 9102","Visa"));
-        mList.add(new ModelDashboard(700,"xxxx xxxx xxxx 7841","Visa"));
-        mList.add(new ModelDashboard(14590,"xxxx xxxx xxxx 4544","Rupay"));
+        mList.add(new ModelDashboard(99999, "xxxx xxxx xxxx 9102", "Visa"));
+        mList.add(new ModelDashboard(700, "xxxx xxxx xxxx 7841", "Visa"));
+        mList.add(new ModelDashboard(14590, "xxxx xxxx xxxx 4544", "Rupay"));
 
 
         // setup viewpager
-        screenPager =findViewById(R.id.screen_viewpager);
-        introViewPagerAdapter = new CustomAdapter(mList,this);
+        screenPager = findViewById(R.id.screen_viewpager);
+        introViewPagerAdapter = new CustomAdapter(mList, this);
         screenPager.setAdapter(introViewPagerAdapter);
+        fontSize = 19;
 
 
         settings = findViewById(R.id.settings);
         settings.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(new Intent(HomeDashboard.this,SettingsActivity.class));
+                startActivity(new Intent(HomeDashboard.this, SettingsActivity.class));
             }
         });
         transfer = findViewById(R.id.transferButton);
@@ -62,5 +66,34 @@ public class HomeDashboard extends Activity {
                 startActivity(intent);
             }
         });
+
+        // TODO: change font size of all text, not just buttons
+
+        services = findViewById(R.id.services_button);
+        plusButton = findViewById(R.id.plus_button);
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fontSize += 2;
+                transfer.setTextSize(fontSize);
+                payment.setTextSize(fontSize);
+                services.setTextSize(fontSize);
+            }
+        });
+
+        // TODO: add minus button to xml file and fix layout
+
+        /*
+        minusButton = findViewById(R.id.minus_button);
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fontSize -= 2;
+                transfer.setTextSize(fontSize);
+                payment.setTextSize(fontSize);
+                services.setTextSize(fontSize);
+            }
+        });
+         */
     }
 }
